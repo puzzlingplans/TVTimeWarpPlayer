3e98d3aa47992e888bd0e3e5eff81fae5bac2662
Steven Hugg <hugg@fasterlight.com>
2016-05-15 02:20:01 +0000

    keyboard arrows to change channel

---
+++ README.md
@@ -0,0 +1,13 @@
+
+This is a console-mode video player for TVTimeWarp.com written in Python for
+the Raspberry Pi.
+
+### INSTALL
+
+    sudo apt-get install omxplayer
+    git clone https://github.com/puzzlingplans/TVTimeWarpPlayer
+
+### RUNNING
+
+    cd TVTimeWarpPlayer
+    python player.py
--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 
 from listing import *
-import subprocess, datetime
+import subprocess, datetime, os, signal, curses, logging
 
 class Player:
 
@@ -9,38 +9,96 @@ class Player:
         self.listing = ChannelListing(self.area)
         self.channel = 57
         self.schedule = None
+        self.proc = None
 
     def get_yturl(self, ytid):
         return subprocess.check_output(['youtube-dl','-g',ytid])
 
-    def next(self):
+    def show_info(self, stdscr, item):
+        stdscr.clear()
+        stdscr.addstr(0, 0, "Channel %d" % self.channel)
+        if item is not None:
+            stdscr.addstr(1, 0, item['name'])
+            stdscr.addstr(2, 0, item['title'])
+            stdscr.addstr(3, 0, '%4.1f mins' % item['duration'])
+        stdscr.refresh()
+
+    def play_next(self, stdscr):
+        self.show_info(stdscr, None)
         t0 = datetime.datetime.now()
+        # load schedule if it is empty
         if self.schedule is None or len(self.schedule) == 0:
             self.schedule = self.listing.get_schedule_for_time(self.channel, t0)
+            logging.debug("Loaded %d items" % len(self.schedule))
+        # get next schedule item
         nextitem = self.schedule[0]
         self.schedule = self.schedule[1:]
-        print(nextitem)
+        logging.debug(nextitem)
+        # get streaming url
         url = nextitem.get('streamUrl')
         if url is None and nextitem['id'][0:3] == 'yt:':
             url = self.get_yturl(nextitem['id'][3:])
+        # find out how many seconds to skip into video
         tmins = get_time_mins(t0)
         skipsecs = (tmins - nextitem['time']) * 60.0
-        self.play(url, skipsecs)
+        # start player
+        self.proc = self.play(url, skipsecs)
+        # show info
+        self.show_info(stdscr, nextitem)
+
+    def is_playing(self):
+        return self.proc.poll() is None
+    
+    def set_channel(self, channel):
+        if channel < self.area.minChannel:
+            channel = self.area.maxChannel-1
+        elif channel > self.area.maxChannel:
+            channel = self.area.minChannel
+        if channel != self.channel:
+            self.channel = channel
+            self.schedule = None
+            logging.debug('channel %d' % self.channel)
+            self.stop()
+
+    def stop(self):
+        if self.proc is not None:
+            logging.debug("stop process %r" % self.proc)
+            os.killpg(os.getpgid(self.proc.pid), signal.SIGINT)
+            #self.proc.terminate()
 
 class OmxPlayer(Player):
 
     def play(self, url, skipsecs):
-        subprocess.call(['omxplayer', '--pos', str(skipsecs), url])
+        return subprocess.Popen(['omxplayer', '--no-osd', '--no-keys', '--pos', str(skipsecs), url],
+            preexec_fn=os.setsid, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 class FFMPEGPlayer(Player):
 
     def play(self, url, skipsecs):
-        subprocess.call(['ffplay', '-ss', str(skipsecs), url])
+        return subprocess.Popen(['ffplay', '-ss', str(skipsecs), url],
+            preexec_fn=os.setsid, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 ###
 
 player = OmxPlayer()
-while True:
-    player.next()
 
+def main(stdscr):
+    curses.halfdelay(2)
+    while True:
+        player.play_next(stdscr)
+        while player.is_playing():
+            ch = stdscr.getch()
+            if ch >= 0:
+                if ch == 259 or ch == 261:
+                    player.set_channel(player.channel + 1)
+                elif ch == 258 or ch == 260:
+                    player.set_channel(player.channel - 1)
+                elif ch == 27:
+                    return
+                else:
+                    logging.debug(str(ch))
 
+try:
+    curses.wrapper(main)
+finally:
+    player.stop()
